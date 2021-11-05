e06c58d7e417f803e539542f43166c046a6cf215
Katie Pundt <kzpundt@gmail.com>
2021-11-05 09:59:42 -0700

    add code for Pizza.prototype.getPrice, corresponding test returns expected results

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,24 @@ function Pizza(size, toppings) {
 
 Pizza.prototype.addTopping  = function(topping) {
   this.toppings.push(topping);
-}
+};
+
+Pizza.prototype.getPrice = function() {
+  let price = 0;
+
+  if (this.size === "s") {
+    price = 15;
+  } else if (this.size === "m") {
+    price = 18;
+  } else if (this.size === "l") {
+    price = 21;
+  } else if (this.size === "xl") {
+    price = 24
+  } else {
+    return "Choose a size!"
+  }
+
+  return price;
+};
 
 // UI logic
